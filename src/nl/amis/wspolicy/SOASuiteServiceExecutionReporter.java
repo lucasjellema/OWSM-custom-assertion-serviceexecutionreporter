8ef02d11672b6ec41f7bc72d40ab4023320a6005
lucasjellema <lucasjellema@gmail.com>
2015-04-02 06:39:59 +0200

    Discern between request and response. Only determine operation and retrieve payload details for request stage.

--- src/nl/amis/wspolicy/SOASuiteServiceExecutionReporter.java
+++ src/nl/amis/wspolicy/SOASuiteServiceExecutionReporter.java
@@ -72,96 +72,101 @@ public class SOASuiteServiceExecutionReporter extends CustomAssertion {
             //                               super.getPolicyBindingProperty("messageTypeOperationsMapping"));
             timestamp = (new Date()).getTime() + "";
             System.out.println("Timestamp " + timestamp);
-            //            String payloadXpath = super.getPolicyBindingProperty("payloadElementXpath");
-            String operationsMap = super.getPolicyBindingProperty("operationsMap");
-            // find out message type
 
-            final HashMap<String, String> namespaces = new HashMap<String, String>();
-            namespaces.put("soap", "http://schemas.xmlsoap.org/soap/envelope/");
-            System.out.println("start investigating message nodes");
-            try {
-                Node messageNode = getDataNode(mBody, namespaces, "/soap:Envelope/soap:Body/*[1]");
-                messageType = messageNode.getLocalName();
-                System.out.println("Message Type = " + messageType);
-                System.out.println("messagnode level 2  " + messageNode.getLocalName() + "--" +
-                                   messageNode.getNodeName() + " pref" + messageNode.getPrefix());
-                //                messageNode = getDataNode(mBody, namespaces, "//soap:Body/*[1]");
-                //                System.out.println("messagnode level 3 " + messageNode.getLocalName() + "--" +
-                //                                   messageNode.getNodeName() + " pref" + messageNode.getPrefix());
-            } catch (Exception e) {
-                System.out.println("exception in bepaling messageNode " + e.getMessage());
-                e.printStackTrace();
-            }
+            if (stage == IMessageContext.STAGE.request) {
 
-            if (operationsMap != null && operationsMap.length() > 1) {
+                //            String payloadXpath = super.getPolicyBindingProperty("payloadElementXpath");
+                String operationsMap = super.getPolicyBindingProperty("operationsMap");
+                // find out message type
 
-                System.out.println("start look at operationsMap exploraration: " + operationsMap);
-
+                final HashMap<String, String> namespaces = new HashMap<String, String>();
+                namespaces.put("soap", "http://schemas.xmlsoap.org/soap/envelope/");
+                System.out.println("start investigating message nodes");
                 try {
-                    JsonReader rdr = Json.createReader(new StringReader(operationsMap));
-                    System.out.println("reader");
-                    JsonObject obj = rdr.readObject();
-                    System.out.println("json obj" + obj);
-                    System.out.println("get object from operations map for " + messageType);
-                    JsonObject operationObj = obj.getJsonObject(messageType);
-                    System.out.println("operation object = " + operationObj);
-
-                    operationName = operationObj.getString("operation");
-
-                    System.out.println("Operation = " + operationName);
-                    String xpathStr = operationObj.getString("xpath");
-                    System.out.println("from json - xpathstring for operation " + xpathStr);
-
-
-                    JsonArray results = operationObj.getJsonArray("namespaces");
-                    System.out.println("results array contains : " + results.size() + " namespaces");
-                    for (JsonObject ns : results.getValuesAs(JsonObject.class)) {
-                        System.out.println("add namespace: " + ns.getString("prefix") + ":" +
-                                           ns.getString("namespace") + "  ;;");
-
-                        namespaces.put(ns.getString("prefix"), ns.getString("namespace"));
-                    } //for
-
-                    System.out.println("get data node for " + xpathStr + " - in " + mBody.toString());
-
-                    Node inputNode = getDataNode(mBody, namespaces, xpathStr);
-                    System.out.println("result from getDataNode " + inputNode.toString());
-                    String inputValue = inputNode.getTextContent();
-                    System.out.println("READ FROM REQUEST - payload value " + inputValue);
-                    payloadValue = inputValue;
+                    Node messageNode = getDataNode(mBody, namespaces, "/soap:Envelope/soap:Body/*[1]");
+                    messageType = messageNode.getLocalName();
+                    System.out.println("Message Type = " + messageType);
+                    System.out.println("messagnode level 2  " + messageNode.getLocalName() + "--" +
+                                       messageNode.getNodeName() + " pref" + messageNode.getPrefix());
+                    //                messageNode = getDataNode(mBody, namespaces, "//soap:Body/*[1]");
+                    //                System.out.println("messagnode level 3 " + messageNode.getLocalName() + "--" +
+                    //                                   messageNode.getNodeName() + " pref" + messageNode.getPrefix());
                 } catch (Exception e) {
+                    System.out.println("exception in bepaling messageNode " + e.getMessage());
                     e.printStackTrace();
-                    System.out.println(e.getMessage());
+                }
+
+                if (operationsMap != null && operationsMap.length() > 1) {
+
+                    System.out.println("start look at operationsMap exploraration: " + operationsMap);
+
+                    try {
+                        JsonReader rdr = Json.createReader(new StringReader(operationsMap));
+                        System.out.println("reader");
+                        JsonObject obj = rdr.readObject();
+                        System.out.println("json obj" + obj);
+                        System.out.println("get object from operations map for " + messageType);
+                        JsonObject operationObj = obj.getJsonObject(messageType);
+                        System.out.println("operation object = " + operationObj);
+
+                        operationName = operationObj.getString("operation");
+
+                        System.out.println("Operation = " + operationName);
+                        String xpathStr = operationObj.getString("xpath");
+                        System.out.println("from json - xpathstring for operation " + xpathStr);
+
+
+                        JsonArray results = operationObj.getJsonArray("namespaces");
+                        System.out.println("results array contains : " + results.size() + " namespaces");
+                        for (JsonObject ns : results.getValuesAs(JsonObject.class)) {
+                            System.out.println("add namespace: " + ns.getString("prefix") + ":" +
+                                               ns.getString("namespace") + "  ;;");
+
+                            namespaces.put(ns.getString("prefix"), ns.getString("namespace"));
+                        } //for
+
+                        System.out.println("get data node for " + xpathStr + " - in " + mBody.toString());
+
+                        Node inputNode = getDataNode(mBody, namespaces, xpathStr);
+                        System.out.println("result from getDataNode " + inputNode.toString());
+                        String inputValue = inputNode.getTextContent();
+                        System.out.println("READ FROM REQUEST - payload value " + inputValue);
+                        payloadValue = inputValue;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
-
 
         }
 
 
-        System.out.println("Service Execution Report");
-        System.out.println("========================");
-        System.out.println("* service: " + serviceId);
-        System.out.println("* operation: " + operationName);
-        System.out.println("* stage: "+stage);
-        System.out.println("* payload: " + payloadValue);
-        System.out.println("* timestamp: " + timestamp);
-        System.out.println("* ecid: " + ecid);
-        System.out.println("========================");
-        System.out.println("");
-
-        if (stage != IMessageContext.STAGE.request) {
+        if (stage == IMessageContext.STAGE.request) {
+            System.out.println("Service Execution Report");
+            System.out.println("========================");
+            System.out.println("* service: " + serviceId);
+            System.out.println("* operation: " + operationName);
+            System.out.println("* stage: " + stage);
+            System.out.println("* payload: " + payloadValue);
+            System.out.println("* timestamp: " + timestamp);
+            System.out.println("* ecid: " + ecid);
+            System.out.println("========================");
+            System.out.println("");
             result.setStatus(IResult.SUCCEEDED);
             return result;
         }
 
-        if (stage != IMessageContext.STAGE.response) {
+        if (stage == IMessageContext.STAGE.response) {
+            System.out.println("Service Execution Report");
+            System.out.println("========================");
+            System.out.println("* service: " + serviceId);
+            System.out.println("* stage: " + stage);
+            System.out.println("* timestamp: " + timestamp);
+            System.out.println("* ecid: " + ecid);
+            System.out.println("========================");
+            System.out.println("");
             result.setStatus(IResult.SUCCEEDED);
-            if (debug) {
-                System.out.println("stage = " + stage);
-                System.out.println(mTag + "Nothing to process in this stage. Returning");
-                System.out.println(mTag + DEBUG_END);
-            }
             return result;
         }
 
